79b58cc052003fb34b3ab7fc4b3a90b42a93db01
Ivan Perez <63859145+ivanz8@users.noreply.github.com>
2023-11-08 22:17:15 +0800

    Update flop.js

--- flop.js
+++ flop.js
@@ -11,18 +11,11 @@ var bird = {
 
 var pipes = [
  {
-  x: 200,
-  y: 0,
-  width: 50,
-  height: 200,
-  speed: 2
- },
- {
-  x: 400,
-  y: 0,
-  width: 50,
-  height: 200,
-  speed: 3
+ x: 200,
+ y: 0,
+ width: 50,
+ height: 200,
+ speed: 2
  }
 ];
 
@@ -37,26 +30,43 @@ function update() {
  bird.y += bird.speed;
 
  for (var i = 0; i < pipes.length; i++) {
-   pipes[i].x -= pipes[i].speed;
+ pipes[i].x -= pipes[i].speed;
 
-   if (bird.y > canvas.height || bird.x < pipes[i].x + pipes[i].width && bird.x + bird.width > pipes[i].x &&
-     bird.y < pipes[i].y + pipes[i].height && bird.y + bird.height > pipes[i].y) {
-     gameOver();
-   }
+ if (bird.y > canvas.height || bird.x < pipes[i].x + pipes[i].width && bird.x + bird.width > pipes[i].x &&
+   bird.y < pipes[i].y + pipes[i].height && bird.y + bird.height > pipes[i].y) {
+   gameOver();
+ }
+
+ if (pipes[i].x < 0) {
+   pipes.shift();
+   pipes.push({
+     x: canvas.width,
+     y: Math.floor(Math.random() * (canvas.height - 200)),
+     width: 50,
+     height: 200,
+     speed: 2 + Math.floor(Math.random() * 3)
+   });
+ }
  }
 }
 
+function drawBird() {
+ ctx.fillStyle = 'yellow';
+ ctx.fillRect(bird.x, bird.y, bird.width, bird.height);
+}
+
+function drawPipes() {
+ for (var i = 0; i < pipes.length; i++) {
+ ctx.fillStyle = 'green';
+ ctx.fillRect(pipes[i].x, pipes[i].y, pipes[i].width, pipes[i].height);
+ }
+}
 
 function draw() {
  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
- ctx.fillStyle = 'yellow';
- ctx.fillRect(bird.x, bird.y, bird.width, bird.height);
-
- for (var i = 0; i < pipes.length; i++) {
-   ctx.fillStyle = 'green';
-   ctx.fillRect(pipes[i].x, pipes[i].y, pipes[i].width, pipes[i].height);
- }
+ drawBird();
+ drawPipes();
+ requestAnimationFrame(draw);
 }
 
 function gameOver() {
@@ -65,16 +75,10 @@ function gameOver() {
  bird.speed = 0;
 
  for (var i = 0; i < pipes.length; i++) {
-  pipes[i].x = 200 + i * 200;
-  pipes[i].y = 0;
-  pipes[i].speed = 2 + i;
+ pipes[i].x = 200 + i * 200;
+ pipes[i].y = 0;
+ pipes[i].speed = 2 + i;
  }
 }
 
-function gameLoop() {
- update();
- draw();
- requestAnimationFrame(gameLoop);
-}
-
-gameLoop();
+draw();
