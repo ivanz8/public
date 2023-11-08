be82df456f51ec9c3bf44dfa6df4fe4c1ceb13b2
Ivan Perez <63859145+ivanz8@users.noreply.github.com>
2023-11-08 22:10:58 +0800

    Update flop.js

--- flop.js
+++ flop.js
@@ -9,17 +9,26 @@ var bird = {
  speed: 0
 };
 
-var pipe = {
- x: 200,
- y: 0,
- width: 50,
- height: 200,
- speed: 2
-};
+var pipes = [
+ {
+  x: 200,
+  y: 0,
+  width: 50,
+  height: 200,
+  speed: 2
+ },
+ {
+  x: 400,
+  y: 0,
+  width: 50,
+  height: 200,
+  speed: 3
+ }
+];
 
 window.onkeydown = function(event) {
  if (event.keyCode === 32) {
-   bird.speed = -5;
+ bird.speed = -5;
  }
 };
 
@@ -27,11 +36,13 @@ function update() {
  bird.speed += 0.5;
  bird.y += bird.speed;
 
- pipe.x -= pipe.speed;
+ for (var i = 0; i < pipes.length; i++) {
+  pipes[i].x -= pipes[i].speed;
 
- if (bird.y > canvas.height || bird.x < pipe.x + pipe.width && bird.x + bird.width > pipe.x &&
-   bird.y < pipe.y + pipe.height && bird.y + bird.height > pipe.y) {
-   gameOver();
+  if (bird.y > canvas.height || bird.x < pipes[i].x + pipes[i].width && bird.x + bird.width > pipes[i].x &&
+    bird.y < pipes[i].y + pipes[i].height && bird.y + bird.height > pipes[i].y) {
+    gameOver();
+  }
  }
 }
 
@@ -41,8 +52,10 @@ function draw() {
  ctx.fillStyle = 'yellow';
  ctx.fillRect(bird.x, bird.y, bird.width, bird.height);
 
- ctx.fillStyle = 'green';
- ctx.fillRect(pipe.x, pipe.y, pipe.width, pipe.height);
+ for (var i = 0; i < pipes.length; i++) {
+  ctx.fillStyle = 'green';
+  ctx.fillRect(pipes[i].x, pipes[i].y, pipes[i].width, pipes[i].height);
+ }
 }
 
 function gameOver() {
@@ -50,9 +63,11 @@ function gameOver() {
  bird.y = 50;
  bird.speed = 0;
 
- pipe.x = 200;
- pipe.y = 0;
- pipe.speed = 2;
+ for (var i = 0; i < pipes.length; i++) {
+  pipes[i].x = 200 + i * 200;
+  pipes[i].y = 0;
+  pipes[i].speed = 2 + i;
+ }
 }
 
 function gameLoop() {
