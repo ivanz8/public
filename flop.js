139f5ed9c14d7fa19a4dfea875245cb93f7eacf0
Ivan Perez <63859145+ivanz8@users.noreply.github.com>
2023-11-08 20:49:50 +0800

    Update flop.js

--- flop.js
+++ flop.js
@@ -1,1 +1,37 @@
 
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
+const bird = {
+    x: 50,
+    y: canvasHeight / 2,
+    radius: 20,
+    velocity: 0,
+    gravity: 0.5,
+};
+
+function drawBird() {
+    ctx.beginPath();
+    ctx.arc(bird.x, bird.y, bird.radius, 0, Math.PI * 2);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+}
+function gameLoop() {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    bird.velocity += bird.gravity;
+    bird.y += bird.velocity;
+
+    drawBird();
+
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
+document.addEventListener("keydown", (event) => {
+    if (event.key === " " || event.key === "ArrowUp") {
+        bird.velocity = -10; // Adjust the jump strength as needed
+    }
+});
